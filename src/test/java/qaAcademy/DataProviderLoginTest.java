87cd949ad1a3070f70274a2de70e69b6de787ae9
rgoyani <rgoyani@gmail.com>
2021-04-12 19:44:54 -0500

    check

--- src/test/java/qaAcademy/DataProviderLoginTest.java
+++ src/test/java/qaAcademy/DataProviderLoginTest.java
@@ -38,7 +38,7 @@ public class DataProviderLoginTest extends base{
 	    lgn.enterPassword().sendKeys(Password);
 	    lgn.clickLogin().click();
 	    log.info(Text);
-	    log.info(Text);
+	
 	   
 	
 	}
